fe9ba70f149f446429fb8d1e00dde73393244087
lottierocuet <lottie.rocuet@gmail.com>
2022-04-26 22:13:24 +0200

    JS POP UP OK CSS KO

--- Experiences.php
+++ Experiences.php
@@ -36,33 +36,7 @@
 
 <body>
   
-<script>
-// Get the modal
-var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-</script>
 
   <div class ="contenant">
 
@@ -134,21 +108,25 @@ window.onclick = function(event) {
       </a>       
 
       <a id="Modif">      
-        <button type="button" onclick=PopupPage() id="myBtn">
+        <button id="myBtn">
           <img src="images/modifier.png"alt="Lien vers site externe"/>
         </button>   
       </a>   
-
-      </div> 
-
-    <div id="myModal" class="modal">
-      <div class="modal-content">
-        Hello :)
-        <!-- <?php //include "PopUpModifPages.php"?> -->
-        <span class="close">&times;</span>
-      </div>
-    </div>
+</div>
       
+
+      <div id="myModal" class="modal">
+        <div class="modal-content">
+          
+         
+          <span class="close">FERMER</span>
+          <?php include "PopUpModifPages.php"?> 
+        
+      </div>
+
+
+
+    </div>     
     </div> 
 
 
@@ -169,6 +147,34 @@ window.onclick = function(event) {
   
   </div>
   <?php include_once "Footer.php"?> 
+
+  <script>
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+</script>
 
 </body>
 </html>
